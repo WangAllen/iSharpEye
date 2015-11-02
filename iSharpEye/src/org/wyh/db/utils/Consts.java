53abc84b7e784c69a3183e2ee56fe306aba3f02a
WangAllen <wufu21@gmail.com>
2015-11-02 10:16:36 +0800

    Initial

---
+++ iSharpEye/src/org/wyh/db/utils/Consts.java
@@ -0,0 +1,22 @@
+package org.wyh.db.utils;
+
+public class Consts {
+
+	public static double distance = 10;
+	
+	public static final double EARTH_RADIUS = 6378137.0;
+	
+	public static String userId_key = "userId";
+	
+	public static String poiId_key = "poiId";
+	
+	public static String lat = "latitude";
+	
+	public static String lon = "longitude";
+	
+	public static String tm = "timestamp";
+	
+	public static String checkinFile = "";
+	
+	public static String friendshipFile = "";
+}
